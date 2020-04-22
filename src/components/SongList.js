eb5c025c64599f80efe1926468204ab8a1afe3b4
mholwill <m_holwill@hotmail.co.uk>
2020-04-22 10:13:34 +0100

    Added border on album covers

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -6,7 +6,8 @@ const SongList = (props) => {
     const songItems = props.songs.map(song => {
         return <SongItem 
             key={song.id.attributes['im:id']} 
-            name={song['im:name'].label}artist={song['im:artist'].label} 
+            name={song['im:name'].label} 
+            artist={song['im:artist'].label} 
             image={song['im:image'][2].label} 
             preview={song['im:collection'].link.attributes['href']}>
             </SongItem>
